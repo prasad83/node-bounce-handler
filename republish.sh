13873fa0c92b80e49735c096f56e4b1e37192d85
prasad <prasad@vtiger.com>
2014-01-23 15:45:37 +0530

    Updated republishing script.

--- republish.sh
+++ republish.sh
@@ -2,7 +2,7 @@
 THISDIR=$(cd "$(dirname "$0")"; pwd)
 cd $THISDIR
 
-VERSION=$(grep "version" package.json | sed 's/"version": "//g' | sed 's/",//g')
+VERSION=$(grep "version" package.json | sed 's/"version": "//g' | sed 's/",//g' | sed -e 's/[ \t]*//')
 
 npm unpublish "node-bounce-handler@$VERSION"
 npm publish .
